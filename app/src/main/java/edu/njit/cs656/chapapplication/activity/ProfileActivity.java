90ad8b13a8876aa256067e42654fe90b6c783e8a
Jon-Paul Marnien <jonpaul.marnien@gmail.com>
2017-11-11 17:29:20 -0500

    View profile is functional.

--- app/src/main/java/edu/njit/cs656/chapapplication/activity/ProfileActivity.java
+++ app/src/main/java/edu/njit/cs656/chapapplication/activity/ProfileActivity.java
@@ -8,6 +8,8 @@ import android.view.MenuItem;
 import android.widget.TextView;
 
 import com.firebase.ui.database.FirebaseListAdapter;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.UserInfo;
 
 import edu.njit.cs656.chapapplication.R;
 import edu.njit.cs656.chapapplication.model.Message;
@@ -24,9 +26,15 @@ public class ProfileActivity extends AppCompatActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_chats);
-    TextView messageText = findViewById(R.id.header);
+    setContentView(R.layout.activity_profile);
+    TextView messageText = findViewById(R.id.profile_header);
     messageText.setText("This is the profile view (onCreate)");
+    UserInfo user = FirebaseAuth.getInstance().getCurrentUser();
+    ((TextView) findViewById(R.id.profile_id)).setText(user.getUid());
+    ((TextView) findViewById(R.id.profile_email)).setText(user.getEmail());
+    ((TextView) findViewById(R.id.profile_displayname)).setText(user.getDisplayName());
+    ((TextView) findViewById(R.id.profile_photo)).setText(user.getPhotoUrl() != null ? user.getPhotoUrl().toString() : "");
+
   }
 
   @Override
